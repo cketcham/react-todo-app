f854eb33b328884a3ae32e9c1b896ac2ee9fb3ab
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-10-25 14:20:36 +0000

    edit_file
    # Path: [4. Process .js and .jsx files]/async_each/async_each/edit_file
    # UUID: e8201f6d-3fe0-434d-9533-023d208b4453
    
    # Changed files:
    src/components/ui/TodoList.js

--- src/components/ui/TodoList.js
+++ src/components/ui/TodoList.js
@@ -3,11 +3,12 @@ import Info from './Info';
 import Header from './Header';
 import Footer from './Footer';
 import FilteredList from './FilteredList';
-import {applyFilter, search, FILTER_ACTIVE} from '../../services/filter';
+import { applyFilter, search, FILTER_ACTIVE } from '../../services/filter';
+import type { TodoListProps } from './TodoList-js.types';
 
-export default function TodoList(props) {
-    const {list, filter, mode, query} = props.data;
-    const {addNew, changeFilter, changeStatus, changeMode, setSearchQuery} = props.actions;
+export default function TodoList(props: TodoListProps): JSX.Element {
+    const { list, filter, mode, query } = props.data;
+    const { addNew, changeFilter, changeStatus, changeMode, setSearchQuery } = props.actions;
     const activeItemCount = applyFilter(list, FILTER_ACTIVE).length;
     const items = search(applyFilter(list, filter), query);
 
@@ -15,10 +16,10 @@ export default function TodoList(props) {
         <div className="container">
             <div className="row">
                 <div className="todolist">
-                    <Header {...{addNew, mode, query, setSearchQuery}}/>
-                    <FilteredList {...{items, changeStatus}}/>
-                    <Footer {...{activeItemCount, filter, changeFilter, mode, changeMode}}/>
-                    <Info {...{mode}}/>
+                    <Header {...{ addNew, mode, query, setSearchQuery }} />
+                    <FilteredList {...{ items, changeStatus }} />
+                    <Footer {...{ activeItemCount, filter, changeFilter, mode, changeMode }} />
+                    <Info {...{ mode }} />
                 </div>
             </div>
         </div>
