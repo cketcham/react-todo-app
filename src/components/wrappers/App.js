8ac7e062b54071e3d954c8302d3b73c5c188d630
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-10-25 14:20:37 +0000

    edit_file
    # Path: [4. Process .js and .jsx files]/async_each/async_each/edit_file
    # UUID: b723b797-0212-49c0-bc9a-eb29144fd5af
    
    # Changed files:
    src/components/wrappers/App.js

--- src/components/wrappers/App.js
+++ src/components/wrappers/App.js
@@ -1,14 +1,15 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import TodoList from '../ui/TodoList';
 import StateProvider from './StateProvider';
 import KeyStrokeHandler from './KeyStrokeHandler';
+import type { AppProps } from './App-js.types';
 
-class App extends Component {
+class App extends Component<AppProps> {
     render() {
         return (
             <StateProvider>
                 <KeyStrokeHandler>
-                    <TodoList/>
+                    <TodoList />
                 </KeyStrokeHandler>
             </StateProvider>
         );
