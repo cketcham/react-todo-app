342bb03711c5730ca3a6bf8acf365b18611a907e
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-10-25 14:22:57 +0000

    edit_file
    # Path: [4. Process .js and .jsx files]/async_each/async_each/edit_file
    # UUID: 541bd46b-5422-4647-9cd4-c745f1607c2d
    
    # Changed files:
    src/components/ui/Footer.js

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import Filter from './Filter';
 import ButtonWrapper from './ButtonWrapper';
+import type { FooterProps } from './Footer-js.types';
 
-export default function Footer(props) {
-    const {activeItemCount, filter, changeFilter} = props;
+export default function Footer(props: FooterProps) {
+    const { activeItemCount, filter, changeFilter } = props;
     return (
         <footer className="clearfix">
             <div className="pull-left buttons">
-                <ButtonWrapper {...props}/>
+                <ButtonWrapper {...props} />
             </div>
             <div className="pull-left">
                 {`${activeItemCount} items left`}
             </div>
             <div className="pull-right">
-                <Filter {...{filter, changeFilter}}/>
+                <Filter {...{ filter, changeFilter }} />
             </div>
         </footer>
     );
