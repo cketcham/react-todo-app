cd708752fee6a3a5537ee397ccc4ce8dbec94c2e
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-10-25 14:22:55 +0000

    edit_file
    # Path: [4. Process .js and .jsx files]/async_each/async_each/edit_file
    # UUID: cc2cbc38-19f0-480e-8de1-719dd2e587d4
    
    # Changed files:
    src/components/ui/Filter.js

--- src/components/ui/Filter.js
+++ src/components/ui/Filter.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import {getOptions} from '../../services/filter';
+import type { FilterProps, Options } from './Filter-js.types';
+import { getOptions } from '../../services/filter';
 
-export default function Filter(props) {
-    const options = getOptions();
-    const {filter, changeFilter} = props;
-    const getClass = (key) => (key === filter ? 'selected' : '');
+export default function Filter(props: FilterProps): JSX.Element {
+    const options: Options = getOptions();
+    const { filter, changeFilter } = props;
+    const getClass = (key: string): string => (key === filter ? 'selected' : '');
 
     return (
         <ul className="filters list-unstyled clearfix">
-            {Object.keys(options).map(key => (
+            {Object.keys(options).map((key: string) => (
                 <li key={key}>
                     <a onClick={() => changeFilter(key)} className={getClass(key)}>
                         {options[key]}
