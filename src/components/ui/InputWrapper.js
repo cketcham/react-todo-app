592b4d5ab7f21279f1fc0314a146a5332170a2ed
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-10-25 14:20:36 +0000

    edit_file
    # Path: [4. Process .js and .jsx files]/async_each/async_each/edit_file
    # UUID: e2ecbc06-2a25-4242-be91-004258edcac1
    
    # Changed files:
    src/components/ui/InputWrapper.js

--- src/components/ui/InputWrapper.js
+++ src/components/ui/InputWrapper.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import InputBox from './InputBox';
 import SearchBox from './SearchBox';
-import {MODE_SEARCH, MODE_CREATE} from '../../services/mode';
+import { MODE_SEARCH, MODE_CREATE } from '../../services/mode';
+import type { InputWrapperProps } from './InputWrapper-js.types';
 
-export default function InputWrapper(props) {
-    const {mode, addNew, query, setSearchQuery} = props;
+export default function InputWrapper(props: InputWrapperProps): JSX.Element | null {
+    const { mode, addNew, query, setSearchQuery } = props;
 
     switch (mode) {
         case MODE_CREATE:
-            return <InputBox {...{addNew}}/>;
+            return <InputBox addNew={addNew} />;
 
         case MODE_SEARCH:
-            return <SearchBox {...{query, setSearchQuery}}/>;
+            return <SearchBox query={query} setSearchQuery={setSearchQuery} />;
 
         default:
             return null;
