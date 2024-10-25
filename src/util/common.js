74aceec87823aa8b1a4a6160c6a50304f2cec0a6
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-10-25 14:20:41 +0000

    edit_file
    # Path: [4. Process .js and .jsx files]/async_each/async_each/edit_file
    # UUID: 7cf554c6-418c-479f-b84a-4b9612ae8794
    
    # Changed files:
    src/util/common.js

--- src/util/common.js
+++ src/util/common.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import type { SelectedAttributes } from './common-js.types';
 
 /**
  * Returns a new object with only few attributes of the original object.
  * Note: the attributes/properties will still be bound to the old object.
  *
  * @param  {Object} object     The object.
- * @param  {Array}             Array of selected attributes.
+ * @param  {Array}  attrs      Array of selected attributes.
  * @return {Object}            New object with only the selected attributes.
  */
-export function objectWithOnly(object, attrs) {
-    let newObject = {};
+export function objectWithOnly<T extends object, K extends keyof T>(object: T, attrs: K[]): Pick<T, K> {
+    let newObject = {} as Pick<T, K>;
 
     attrs.forEach(attr => {
         newObject[attr] = object[attr].bind(object);
@@ -21,8 +22,8 @@ export function objectWithOnly(object, attrs) {
 /**
  * Wraps react children elements with props.
  */
-export function wrapChildrenWith(children, props) {
-    return React.Children.map(children, child => React.cloneElement(child, props));
+export function wrapChildrenWith(children: React.ReactNode, props: Record<string, any>): React.ReactNode {
+    return React.Children.map(children, child => React.isValidElement(child) ? React.cloneElement(child, props) : child);
 }
 
 /**
@@ -32,6 +33,6 @@ export function wrapChildrenWith(children, props) {
  * @param  {String} substr
  * @return {Boolean}
  */
-export function stringInclues(str, substr) {
+export function stringIncludes(str: string, substr: string): boolean {
     return str.indexOf(substr) !== -1;
 }
