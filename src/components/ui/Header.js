fe4004554ce690c6f5ab12131b85d68e40c03936
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-10-25 14:22:55 +0000

    edit_file
    # Path: [4. Process .js and .jsx files]/async_each/async_each/edit_file
    # UUID: 8a4e0965-e0fa-400b-b1c4-024d358de49d
    
    # Changed files:
    src/components/ui/Header.js

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import InputWrapper from './InputWrapper';
+import type { HeaderProps } from './Header-js.types';
 
-export default function Header(props) {
+export default function Header(props: HeaderProps): JSX.Element {
     return (
         <header>
             <h1>Things To Do</h1>
-            <InputWrapper {...props}/>
+            <InputWrapper {...props} />
         </header>
     );
 }
